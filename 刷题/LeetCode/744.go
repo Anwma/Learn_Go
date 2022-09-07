d13ec515dd480e5f25a9bf809c522230d39589c3
Anwma <51366315+Anwma@users.noreply.github.com>
2022-09-07 23:43:20 +0800

    22-09-07

---
+++ 刷题/LeetCode/744.go
@@ -0,0 +1,12 @@
+package main
+
+import "sort"
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	//corner case
+	if target >= letters[len(letters)-1] {
+		return letters[0]
+	}
+	i := sort.Search(len(letters)-1, func(i int) bool { return letters[i] > target })
+	return letters[i]
+}
---
+++ 刷题/LeetCode/852.go
@@ -0,0 +1,34 @@
+package main
+
+func peakIndexInMountainArray(arr []int) int {
+	//只需找到临界点 该点前后的二值均小于该值
+	for i := 1; i < len(arr)-1; i++ {
+		if arr[i] > arr[i-1] && arr[i] > arr[i+1] {
+			return i
+		}
+	}
+	return -1
+}
+
+func peakIndexInMountainArray2(a []int) int {
+	// Write your code here
+	l,r := 1,len(a)-1
+	for l + 1 < r {
+		m := l + (r - l) >> 1
+		if a[m] > a[m-1] && a[m] > a[m+1]{
+			return m
+		}else if a[m] < a[m-1] && a[m] > a[m+1]{
+			//右侧
+			r = m
+		}else{
+			l = m
+		}
+	}
+	if a[l] > a[l-1] && a[l] > a[l+1]{
+		return l
+	}
+	if a[r] > a[r-1] && a[r] > a[r+1]{
+		return r
+	}
+	return -1
+}
