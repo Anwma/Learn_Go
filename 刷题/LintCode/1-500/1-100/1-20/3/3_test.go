432ca4bec9a8d10c0977d566aa491eb41cc24445
Anwma <51366315+Anwma@users.noreply.github.com>
2022-09-09 20:06:41 +0800

    22-09-09

--- 刷题/LintCode/1-500/1-100/1-20/3/3_test.go
+++ 刷题/LintCode/1-500/1-100/1-20/3/3_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -9,15 +9,53 @@ import (
 func DigitCounts(k int, n int) int {
 	// write your code here
 	count := 0
-	strK := fmt.Sprintf("%d", k)
+	strK := strconv.Itoa(k)
 	for i := 0; i <= n; i++ {
-		count += len(strings.Split(fmt.Sprintf("%d", i), strK)) - 1
+		count += len(strings.Split(strconv.Itoa(i), strK)) - 1
 	}
 	return count
 }
 
+func DigitCounts1(k int, n int) int {
+	// write your code here
+	str := strconv.Itoa(n)
+	//n的取值范围为0~10^5
+	maxBit := 6
+	tenPow := make([]int, maxBit)
+	tenPow[0] = 1
+	for i := 1; i < maxBit; i++ {
+		tenPow[i] = tenPow[i-1] * 10
+	}
+	res := 0
+	for i := 0; i < len(str); i++ {
+		curNum := int(str[i] - '0')
+		count := 0
+		// i位置前的可能性
+		if i > 0 {
+			if k != 0 {
+				count = n / tenPow[len(str)-i] * tenPow[len(str)-1-i]
+			} else {
+				count = (n/tenPow[len(str)-i] - 1) * tenPow[len(str)-1-i]
+			}
+		}
+		// i位置后面的可能性
+		if i > 0 || k != 0 {
+			if curNum > k {
+				count += tenPow[len(str)-i-1]
+			} else if curNum == k {
+				count += n%tenPow[len(str)-i-1] + 1
+			}
+		}
+		res += count
+	}
+	if k == 0 {
+		res++
+	}
+	return res
+}
+
 func BenchmarkDigitCounts(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		DigitCounts(5, 3)
+		DigitCounts1(1, 11)
 	}
 }
--- 刷题/LintCode/501-1000/601-700/669/669_test.go
+++ 刷题/LintCode/501-1000/601-700/669/669_test.go
@@ -1,27 +1,20 @@
 package _69
 
 import (
+	"math"
 	"testing"
 )
 
-func BenchmarkCoinChange(b *testing.B) {
-	coins := []int{1, 2, 5}
-	num := 27
-	for i := 0; i < b.N; i++ {
-		CoinChange(coins, num)
-	}
-}
 func CoinChange(coins []int, amount int) int {
 	arr := make([]int, amount+1)
 	n := len(coins)
 	arr[0] = 0
 	for i := 1; i <= amount; i++ {
-		arr[i] = 2<<31 - 1
+		arr[i] = math.MaxInt
 		//last coin
 		//arr[i] = min{arr[i-coins[0]]+1, ... , f[i-coins[n-1]] + 1}
 		for j := 0; j < n; j++ {
 			//i 代表当前的钱数 当前钱数要 >= 硬币数组中的值
-			//且
 			if i >= coins[j] {
 				arr[i] = min(arr[i-coins[j]]+1, arr[i])
 			}
@@ -38,4 +31,12 @@ func min(x, y int) int {
 		return y
 	}
 	return x
+}
+
+func BenchmarkCoinChange(b *testing.B) {
+	coins := []int{1, 2, 5}
+	num := 27
+	for i := 0; i < b.N; i++ {
+		CoinChange(coins, num)
+	}
 }
